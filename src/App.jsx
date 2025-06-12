ae5421ce1d2a68a02c264839dee97539de5be069
limo-git <Dimpleyadav32@gmail.com>
2025-06-12 15:13:38 +0530

    fe

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import SignupPage from "./Pages/SignupPage";
 import DataCollectionForm from "./Pages/DataCollectionForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SymptomSelection from "./Pages/SymptomSelection";
+import Analytics from "./Pages/Analytics";
+import CollaborationLayout from "./Pages/CollaborationLayout";
+import CommunicationHub from "./Pages/collaboration_subpages/CommunicationHub.jsx";
+import SocialMediaIntelligence from "./Pages/collaboration_subpages/SocialMediaIntelligence.jsx";
+import AlertManagement from "./Pages/collaboration_subpages/AlertManagement.jsx";
+import ResourceSharing from "./Pages/collaboration_subpages/ResourceSharing.jsx";
+import ResponseCoordination from "./Pages/collaboration_subpages/ResponseCoordination.jsx";
 
 const App = () => (
   <main className="relative">
@@ -14,6 +21,17 @@ const App = () => (
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/symptomSelection" element={<SymptomSelection />}></Route>
+        <Route path="/analytics" element={<Analytics />}></Route>
+        <Route path="/collaboration" element={<CollaborationLayout />}>
+          <Route index element={<CommunicationHub />} />
+          <Route path="communication" element={<CommunicationHub />} />
+          <Route path="social-feed" element={<SocialMediaIntelligence />} />
+          <Route path="alerts" element={<AlertManagement />} />
+          <Route path="resources" element="ResourceSharing" />
+          <Route path="coordination" element="ResponseCoordination" />
+          {/* Analytics tab inside collaboration will link back to main Analytics page */}
+          <Route path="analytics" element={<Analytics />} /> 
+        </Route>
       </Routes>
     </BrowserRouter>
   </main>
